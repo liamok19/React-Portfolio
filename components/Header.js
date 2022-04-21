bf7ffceeaf5853e2c63e1c22b2e3833e2812d470
Liam Ok <liamokane19@gmail.com>
2022-04-21 14:48:41 +1000

    updated the header js with filler text and style

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const styles = {
+    const:{
+        background: 'cyan'
+    },
+}
+
+export default function Header() {
+    return(
+        <header
+        style ={styles.card}
+        className="header">
+            This is a default Header WERQ
+        </header>
+    )
+}
