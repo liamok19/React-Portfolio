7c569c03ec1acbb9456d09843748fb6949d514c0
Liam Ok <liamokane19@gmail.com>
2022-04-21 14:56:01 +1000

    updated footer with basic text and styling

---
+++ components/Footer.js
@@ -0,0 +1,24 @@
+//Pseudocode
+//import React - DONE
+//import styles - DONE
+//create variable to hold the style elements required for this component
+//create a function with the relevant text for this section. Export feault this functio 
+// Inside the function make sure we're returning the styles using {}
+
+
+import React from 'react'
+import '../styles/Footer.css'
+
+const styles = {
+    card: {
+        background: 'red'
+    },
+}
+
+export default function Footer(){
+    return (
+        <footer style={styles.card}>
+            FOOTER bitches
+        </footer>
+    )
+}
