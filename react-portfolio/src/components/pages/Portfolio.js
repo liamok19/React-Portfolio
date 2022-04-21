b5df5b6e3c930aba8adaecf1685e801b0964351b
Liam Ok <liamokane19@gmail.com>
2022-04-21 17:28:04 +1000

    both the resume and portfolio section of the navbar is working. Test text is responding aswell

---
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Portfolio(){
+    return (
+        <>
+        <h1>
+            this is the portfolio section of the website YEEHAW
+        </h1>
+        </>
+    )
+}
---
+++ react-portfolio/src/components/pages/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Resume () {
+    return(
+        <>
+        <h1>
+            this is my Resume yeehaw
+        </h1>
+        </>
+    )
+}
+
