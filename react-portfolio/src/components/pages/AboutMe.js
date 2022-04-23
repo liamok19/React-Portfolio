fa9e4afb4a89414a395d19a8ddd1917ea2040072
Liam Ok <liamokane19@gmail.com>
2022-04-23 11:25:54 +1000

    updated size of the image on about me

--- react-portfolio/src/components/pages/AboutMe.js
+++ react-portfolio/src/components/pages/AboutMe.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import image from '../../images/liamo_headshot_01.JPG'
 
-// const styles = {
-//   card:{
-//     fontSize: 1,
-//     background: 'cyan',
-//   },
-// }
+const styles = {
+  card:{
+    fontSize: 1,
+    background: 'cyan',
+  },
+  imgStyle: {
+    width: 450,
+    height: 300
+  },
+}
 
 
 
 export default function About() {
   return (
     <div >
-      <img src={image} alt="headshot" />
+      <img style={styles.imgStyle} src={image} alt="headshot" />
       <h1>About Page</h1>
       <p >Aloha, I'm Liam, Li, Leeham and my list of nicknames continues to grow. I worked in Visual FX and Animation for over 8 years and have a wealth of experience managing productions involving Artists, Directors, Admin, Production, and Marketing team members. Web Development is a skill I would like to expand.</p> 
       <p>A fabulous thing about me is that I have traded dancing on stage for dancing in the kitchen. Despite the fact that it has no relevance to programming, it contributes to the upbeat nature I bring to the working environment.</p>
