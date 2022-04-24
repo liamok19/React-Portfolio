f281541dd5f27e6b1a31c31fdf8f326c08f71638
Liam Ok <liamokane19@gmail.com>
2022-04-24 13:25:02 +1000

    updated html elements in aboutmepage

--- react-portfolio/src/components/pages/AboutMe.js
+++ react-portfolio/src/components/pages/AboutMe.js
@@ -18,7 +18,7 @@ export default function About() {
   return (
     <div >
       <img style={styles.imgStyle} src={image} alt="headshot" />
-      <h1>About Page</h1>
+      <h1>Bio</h1>
       <p >Aloha, I'm Liam, Li, Leeham and my list of nicknames continues to grow. I worked in Visual FX and Animation for over 8 years and have a wealth of experience managing productions involving Artists, Directors, Admin, Production, and Marketing team members. Web Development is a skill I would like to expand.</p> 
       <p>A fabulous thing about me is that I have traded dancing on stage for dancing in the kitchen. Despite the fact that it has no relevance to programming, it contributes to the upbeat nature I bring to the working environment.</p>
           </div>
